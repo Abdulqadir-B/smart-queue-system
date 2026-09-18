2c216d003da05494c1f4fa6b693baaf776119f18
Abdulqadir <aqbearing@gmail.com>
2026-09-18 11:57:42 +0530

    Refactor to use Data Router API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
@@ -10,19 +9,12 @@ import { QueueProvider } from './context/QueueContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
-    >
-      <ThemeProvider>
-        <SocketProvider>
-          <QueueProvider>
-            <App />
-          </QueueProvider>
-        </SocketProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <SocketProvider>
+        <QueueProvider>
+          <App />
+        </QueueProvider>
+      </SocketProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
